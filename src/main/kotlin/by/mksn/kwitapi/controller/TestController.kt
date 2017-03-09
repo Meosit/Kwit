20a2da9013c1ea292b1b9af9bf357544814322ad
Mikhail_Snitavets <mksn13@gmail.com>
2017-03-09 15:47:33 +0300

    Add basic oauth2 configuration (isn't working yet)

--- src/main/kotlin/by/mksn/kwitapi/controller/TestController.kt
+++ src/main/kotlin/by/mksn/kwitapi/controller/TestController.kt
@@ -1,8 +1,11 @@
 package by.mksn.kwitapi.controller
 
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
 
-@ResponseBody
+@RestController
 @RequestMapping("/api")
 class TestController {
 
