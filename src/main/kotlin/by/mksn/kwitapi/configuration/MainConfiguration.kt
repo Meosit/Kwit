63c575c2a717fc44beab8d3755f68b8a5b472aec
Mikhail Snitovets <mksn13@gmail.com>
2017-03-10 23:20:08 +0300

    Remove dumb configurations

--- src/main/kotlin/by/mksn/kwitapi/configuration/MainConfiguration.kt
+++ src/main/kotlin/by/mksn/kwitapi/configuration/MainConfiguration.kt
@@ -3,15 +3,15 @@ package by.mksn.kwitapi.configuration
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.support.SpringBootServletInitializer
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 
+
 @Configuration
-@Import(ControllerConfiguration::class,
-        ResourceServerConfiguration::class,
-        AuthorizationServerConfiguration::class,
-        OAuth2SecurityConfiguration::class)
+@Import(ControllerConfiguration::class)
 @EnableAutoConfiguration
+@ComponentScan("by.mksn.kwitapi.configuration")
 open class MainConfiguration : SpringBootServletInitializer() {
 
     override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
