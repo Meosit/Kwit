acfe93c53b4d7d424a94c3b07aad22c543a618c3
Mikhail_Snitavets <mksn13@gmail.com>
2017-03-21 17:04:34 +0300

    Add more entities

--- src/main/kotlin/by/mksn/kwitapi/model/Entities.kt
+++ src/main/kotlin/by/mksn/kwitapi/model/Entities.kt
@@ -5,13 +5,14 @@ import java.sql.Timestamp
 import javax.persistence.*
 
 @Entity
-@Table(name = "user")
+@Table(name = "user", uniqueConstraints =
+arrayOf(UniqueConstraint(columnNames = arrayOf("email"))))
 data class User(
         @Id
         @GeneratedValue(strategy = GenerationType.AUTO)
         @Column(columnDefinition = "INT")
         var id: Long,
-        @Column(columnDefinition = "VARCHAR(255)", nullable = false)
+        @Column(columnDefinition = "VARCHAR(255)", unique = true, nullable = false)
         var email: String,
         @Column(columnDefinition = "CHAR(60)", nullable = false)
         var passwordHash: String,
@@ -20,6 +21,33 @@ data class User(
         @Column(columnDefinition = "DATETIME")
         var createdAt: Timestamp?,
         var isDeleted: Boolean
-) : Serializable {
-    private constructor() : this(0, "", "", null, null, false)
-}
+) : Serializable
+
+@Entity
+@Table(name = "currency")
+data class Currency(
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        @Column(columnDefinition = "INT")
+        var id: Long,
+        @Column(columnDefinition = "CHAR(3)", nullable = false)
+        var code: String,
+        @Column(columnDefinition = "VARCHAR(5)", nullable = false)
+        var symbol: String,
+        var isPrefix: Boolean
+)
+
+@Entity
+@Table(name = "wallet")
+data class Wallet(
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        @Column(columnDefinition = "INT")
+        var id: Long,
+        @Column(columnDefinition = "INT")
+        var userId: Long,
+        @ManyToOne
+        @JoinColumn(name = "currency_id")
+        var currency: Currency,
+        var name: String
+)
